7787829c409c5eb21ef8f9b7d365a324f1792b94
E-Liang Tan <eliang@eliangtan.com>
2018-01-05 20:43:49 +0800

    Add matcher for most SDE rooms

--- matchers.ts
+++ matchers.ts
@@ -1,5 +1,7 @@
 import { Room } from "./match";
 
+// type Matcher = (venueName: string, room: Room) => boolean;
+
 // Tries a simple name match
 function nameMatcher(venueName: string, room: Room) {
   return venueName === room.roomcode;
@@ -22,4 +24,29 @@ function numberSplitMatcher(venueName: string, room: Room) {
   return venueName === adaptedVenueName;
 }
 
-export default [nameMatcher, ltMatcher, numberSplitMatcher];
+function sdeMatcher(venueName: string, room: Room) {
+  if (venueName.indexOf("SDE") !== 0 || room.roomcode.indexOf("SDE") !== 0)
+    return false;
+
+  // Learning rooms
+  const lrMatches = venueName.match(/SDE-(\d{3})/);
+  if (lrMatches) {
+    if (room.roomcode === `SDE3-04-${lrMatches[1]}`) return true;
+  }
+
+  // Seminar rooms
+  const srMatches = venueName.match(/SDE-SR(\d+)/);
+  if (srMatches) {
+    if (room.roomname === `SEMINAR ROOM ${srMatches[1]}`) return true;
+  }
+
+  // E-studios
+  const esMatches = venueName.match(/SDE-ES(\d+)/);
+  if (esMatches) {
+    if (room.roomname === `E-STUDIO ${esMatches[1]}`) return true;
+  }
+
+  return false;
+}
+
+export default [nameMatcher, ltMatcher, numberSplitMatcher, sdeMatcher];
